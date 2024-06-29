2a6a921248125abf94b984ef55292dc91b0f47f5
YanKarpov <KarpovYAA23@spb.ithub.ru>
2024-06-29 16:59:12 +0300

    Обновил интерфейс и логику нейросети для определения ещё двух эмоций

--- scripts/NeuralNetwork.js
+++ scripts/NeuralNetwork.js
@@ -1,8 +1,5 @@
-/**
- * Создает нейронную сеть для распознавания эмоций на изображениях 10x10 пикселей.
- * Модель состоит из сверточного слоя, слоя максимального пулинга, слоя выравнивания и полносвязного слоя для классификации.
- */
 const model = tf.sequential();
+
 model.add(
   tf.layers.conv2d({
     inputShape: [10, 10, 1],
@@ -13,35 +10,19 @@ model.add(
 );
 model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
 model.add(tf.layers.flatten());
-model.add(tf.layers.dense({ units: 2, activation: "softmax" }));
+model.add(tf.layers.dense({ units: 4, activation: "softmax" })); // Изменено на 4 нейрона для 4 классов
 
-/**
- * Компилирует нейронную сеть с оптимизатором Adam, функцией потерь categoricalCrossentropy
- * и метриками accuracy для оценки производительности модели во время обучения.
- */
 model.compile({
   optimizer: "adam",
   loss: "categoricalCrossentropy",
   metrics: ["accuracy"],
 });
 
-/**
- * Преобразует рисунок, нарисованный на холсте, в 4D тензор для входа в нейронную сеть.
- * @param {number[][]} paintField - Массив пикселей рисунка 10x10 в оттенках серого.
- * @returns {tf.Tensor4D} 4D тензор с данными рисунка для входа в модель.
- */
 function preprocessDrawing(paintField) {
   const tensor = tf.tensor4d(paintField, [1, 10, 10, 1]);
   return tensor;
 }
 
-/**
- * Обучает нейронную сеть на предоставленных данных.
- * @param {Object[]} trainData - Массив объектов, содержащих данные рисунков и их метки.
- * @param {number[][]} trainData[].paintField - Рисунок в виде массива пикселей 10x10.
- * @param {number[]} trainData[].label - Метка класса (0 или 1).
- * @returns {Promise<void>} Promise, который завершается после завершения обучения модели.
- */
 async function trainModel(trainData) {
   const xs = tf.concat(trainData.map((d) => preprocessDrawing(d.paintField)));
   const ys = tf.tensor2d(trainData.map((d) => d.label));
@@ -50,13 +31,12 @@ async function trainModel(trainData) {
     epochs: 100,
     callbacks: {
       onEpochEnd: (epoch, logs) => {
-        console.log(
-          `Эпоха ${epoch}: потеря = ${logs.loss.toFixed(4)}, точность = ${logs.acc.toFixed(4)}`
-        );
+        console.log(`Эпоха ${epoch}: потеря = ${logs.loss.toFixed(4)}, точность = ${logs.acc.toFixed(4)}`);
       },
     },
   });
 
   console.log("Обучение модели завершено");
 }
+
 
