aac27ce2f0ae902bdeabf0166f6634e1f5c8df9e
Yan <example@email.com>
2024-07-01 20:43:48 +0300

    Implemented asynchronous operations for prediction and training buttons

--- scripts/EmotionDrawing.js
+++ scripts/EmotionDrawing.js
@@ -1,4 +1,3 @@
-// Получение элементов canvas и кнопок
 const canvas = document.querySelector("#paintField");
 const clearBtn = document.querySelector("#clear");
 const sadBtn = document.querySelector("#sad");
@@ -20,7 +19,6 @@ const paintField = new Array(100).fill(false);
 // Массив данных для обучения модели
 const trainData = [];
 
-// Переменные для отслеживания состояния интерфейса
 let mouseDown = false;
 let happyCount = 0;
 let sadCount = 0;
@@ -31,7 +29,7 @@ let selectedColor = colorPicker.value;
 /**
  * Рисует сетку на canvas.
  */
-function drawGrid() {
+const drawGrid = () => {
   ctx.strokeStyle = "#CCC";
 
   for (let i = 1; i < 10; i++) {
@@ -42,16 +40,16 @@ function drawGrid() {
   }
 
   ctx.stroke();
-}
+};
 
 /**
  * Очищает canvas и перерисовывает сетку.
  */
-function clearCanvas() {
+const clearCanvas = () => {
   ctx.fillStyle = "#FFF";
   ctx.fillRect(0, 0, 400, 400);
   drawGrid();
-}
+};
 
 /**
  * Рисует квадрат заданного цвета в указанной ячейке на canvas.
@@ -59,16 +57,18 @@ function clearCanvas() {
  * @param {number} column - Номер столбца ячейки
  * @param {string} color - Цвет для заливки квадрата
  */
-function drawSquare(row, column, color) {
+const drawSquare = (row, column, color) => {
   ctx.fillStyle = color;
   ctx.fillRect(column * 40 + 1, row * 40 + 1, 38, 38);
-}
+};
 
 /**
  * Обработчик события для рисования на canvas при движении мыши.
  * @param {MouseEvent} event - Событие мыши
  */
-function draw(event) {
+const draw = (event) => {
+  if (!mouseDown) return;
+
   const offsetX = Math.min(Math.max(event.offsetX, 0), 399);
   const offsetY = Math.min(Math.max(event.offsetY, 0), 399);
 
@@ -76,35 +76,33 @@ function draw(event) {
   const columnIndex = Math.floor(offsetX / 40);
   const arrayIndex = rowIndex * 10 + columnIndex;
 
-  if (arrayIndex >= 0 && arrayIndex < paintField.length) {
+  if (
+    arrayIndex >= 0 &&
+    arrayIndex < paintField.length &&
+    !paintField[arrayIndex]
+  ) {
     paintField[arrayIndex] = true;
     drawSquare(rowIndex, columnIndex, selectedColor);
   }
-}
+};
 
-/**
- * Очищает поле рисования (paintField).
- */
-function clearField() {
+const clearField = () => {
   paintField.fill(false);
   clearCanvas();
-}
+};
 
-/**
- * Обновляет текст кнопок для отображения текущего количества счастливых, грустных, злых и удивленных рисунков.
- */
-function updateInterface() {
+const updateInterface = () => {
   happyBtn.innerText = `😀 ${happyCount}`;
   sadBtn.innerText = `🙁 ${sadCount}`;
   angryBtn.innerText = `😡 ${angryCount}`;
   surprisedBtn.innerText = `😲 ${surprisedCount}`;
-}
+};
 
 /**
  * Сохраняет текущее состояние рисунка и его метку в массив данных обучения.
  * @param {number[]} value - Метка рисунка (например, [1, 0, 0, 0] для счастливого, [0, 1, 0, 0] для грустного)
  */
-function storeResult(value) {
+const storeResult = (value) => {
   trainData.push({ paintField: [...paintField], label: value });
   if (value[0] === 1) happyCount++;
   else if (value[1] === 1) sadCount++;
@@ -112,17 +110,17 @@ function storeResult(value) {
   else if (value[3] === 1) surprisedCount++;
   updateInterface();
   clearField();
-}
+};
 
 // Сохраняет данные обучения в файл JSON
-function saveTrainingData() {
+const saveTrainingData = () => {
   const jsonData = JSON.stringify(trainData);
-  const blob = new Blob([jsonData], { type: 'application/json' });
+  const blob = new Blob([jsonData], { type: "application/json" });
   const url = URL.createObjectURL(blob);
 
-  const a = document.createElement('a');
+  const a = document.createElement("a");
   a.href = url;
-  a.download = 'trainData.json';
+  a.download = "trainData.json";
   document.body.appendChild(a);
   a.click();
 
@@ -131,41 +129,43 @@ function saveTrainingData() {
     document.body.removeChild(a);
     alert("Данные обучения сохранены в файл trainData.json!");
   }, 0);
-}
+};
 
 // Загружает данные обучения из файла JSON
-function loadTrainingData(event) {
+const loadTrainingData = (event) => {
   const file = event.target.files[0];
   const reader = new FileReader();
 
-  reader.onload = function (e) {
-    const jsonData = e.target.result;
-    const loadedData = JSON.parse(jsonData);
-    trainData.push(...loadedData);
+  reader.onload = (e) => {
+    try {
+      const jsonData = e.target.result;
+      const loadedData = JSON.parse(jsonData);
+      trainData.push(...loadedData);
 
-    loadedData.forEach(data => {
-      if (data.label[0] === 1) happyCount++;
-      if (data.label[1] === 1) sadCount++;
-      if (data.label[2] === 1) angryCount++;
-      if (data.label[3] === 1) surprisedCount++;
-    });
+      loadedData.forEach((data) => {
+        if (data.label[0] === 1) happyCount++;
+        if (data.label[1] === 1) sadCount++;
+        if (data.label[2] === 1) angryCount++;
+        if (data.label[3] === 1) surprisedCount++;
+      });
 
-    updateInterface();
-    alert("Данные обучения загружены!");
+      updateInterface();
+      alert("Данные обучения загружены!");
+    } catch (error) {
+      alert("Ошибка при загрузке данных: " + error.message);
+    }
   };
 
   reader.readAsText(file);
-}
+};
 
-loadInput.addEventListener('change', loadTrainingData);
+loadInput.addEventListener("change", loadTrainingData);
 
 // Добавление обработчиков событий для взаимодействия с мышью и кнопками
 document.addEventListener("mousedown", () => (mouseDown = true));
 document.addEventListener("mouseup", () => (mouseDown = false));
 
-canvas.addEventListener("mousemove", (e) => {
-  if (mouseDown) draw(e);
-});
+canvas.addEventListener("mousemove", draw);
 
 clearBtn.addEventListener("click", clearField);
 
@@ -185,25 +185,28 @@ surprisedBtn.addEventListener("click", () => {
   storeResult([0, 0, 0, 1]);
 });
 
-predictBtn.addEventListener("click", () => {
+predictBtn.addEventListener("click", async () => {
   const inputTensor = preprocessDrawing(paintField);
   const prediction = model.predict(inputTensor);
-  const [happiness, sadness, anger, surprise] = prediction.dataSync();
+  const [happiness, sadness, anger, surprise] = await prediction.data();
 
   const emotions = ["счастливое", "грустное", "злое", "удивленное"];
   const scores = [happiness, sadness, anger, surprise];
   const maxIndex = scores.indexOf(Math.max(...scores));
 
   alert(
-    `Я думаю это ${emotions[maxIndex]} лицо!\nСчастье: ${Math.round(happiness * 100)}% Грусть: ${Math.round(sadness * 100)}% Злость: ${Math.round(anger * 100)}% Удивление: ${Math.round(surprise * 100)}%`
+    `Я думаю это ${emotions[maxIndex]} лицо!\nСчастье: ${Math.round(
+      happiness * 100
+    )}% Грусть: ${Math.round(sadness * 100)}% Злость: ${Math.round(
+      anger * 100
+    )}% Удивление: ${Math.round(surprise * 100)}%`
   );
 });
 
-trainBtn.addEventListener("click", () => {
-  trainModel(trainData).then(() => {
-    predictBtn.disabled = false;
-    alert("Я обучился!");
-  });
+trainBtn.addEventListener("click", async () => {
+  await trainModel(trainData);
+  predictBtn.disabled = false;
+  alert("Я обучился!");
 });
 
 saveBtn.addEventListener("click", saveTrainingData);
